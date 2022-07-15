e63e4422bf0da4441b5724b4f46af92185e70ba0
lv-Ferreira <lvferreira022@gmail.com>
2022-07-14 23:45:38 -0300

    cria bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: lightTheme,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }

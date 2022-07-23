e4bba828cb1a854a0f200f6c7d6bcfb9d93ac4fd
Washiro1 <washington_m_n@hotmail.com>
2022-07-23 17:49:37 -0300

    Adicionando biblioteca de geolocalização e testes

---
+++ lib/commons/localizacao/localizacao.controller.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LocalizacaoController extends ChangeNotifier {
+  double lat = 0.0;
+  double long = 0.0;
+  String erro = '';
+
+  LocalizacaoController() {
+    getPosicao();
+  }
+
+  getPosicao() async {
+    try {
+      Position posicao = await _posicaoAtual();
+      lat = posicao.latitude;
+      long = posicao.longitude;
+    } catch (e) {
+      erro = e.toString();
+    }
+    notifyListeners();
+  }
+
+  Future<Position> _posicaoAtual() async {
+    LocationPermission permissao;
+
+    bool ativado = await Geolocator.isLocationServiceEnabled();
+    if (!ativado) {
+      return Future.error('Por favor, habilite a localização no smartphone');
+    }
+
+    permissao = await Geolocator.checkPermission();
+    if (permissao == LocationPermission.denied) {
+      permissao = await Geolocator.requestPermission();
+      if (permissao == LocationPermission.denied) {
+        return Future.error('Você precisa autorizar o acesso à localização');
+      }
+    }
+
+    if (permissao == LocationPermission.deniedForever) {
+      return Future.error('Você precisa autorizar o acesso à localização');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+}
